4cca7ef1328b6ccaf14b3a19ea85effc4295b5be
Victor-Matoso <victorrodriguesmjau@gmail.com>
2023-06-22 14:26:01 -0300

    Co-authored-by: B4g4rini <B4g4rini@users.noreply.github.com> Co-authored-by: Eduardo Filippi <EduFilippi@users.noreply.github.com> Co-authored-by: Renan Fernando Ruiz <RenanFernandoRuiz@users.noreply.github.com> Co-authored-by: Rabaka <Rabaka07@users.noreply.github.com> Co-authored-by: di0rio <di0rio@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-// function cadastroAlerta(){
-//     if(){
-//         alert("Enviado!")
+// function cadastroAlerta() {
+  
+//     if (nome === '' || email === '' || senha === '') {
+//       alert('Por favor, preencha todos os campos.');
+//       return;
 //     }
-// }
+
+//     alert('Cadastro realizado com sucesso!');
+    
+//     document.getElementById('input[name="nome"]').value = '';
+//     document.getElementById('input[name="email"]').value = '';
+//     document.getElementById('input[name="senha"]').value = '';
+//   }
+  
+
+//-----------------------------------------------------------//
